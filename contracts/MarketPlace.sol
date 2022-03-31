330f3acc942bd662a2a2f4de485263d77e0f0ad4
sandeep-chahal <sandeepchahal573@gmail.com>
2022-03-31 19:55:11 +0530

    created MarketPlace contract created tests for MarketPlace contract

---
+++ contracts/MarketPlace.sol
@@ -0,0 +1,126 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/utils/Strings.sol";
+// import "hardhat/console.sol";
+import "./CryptoChitahs.sol";
+
+contract MarketPlace {
+    address payable public owner;
+    CryptoChitahs public nft;
+    bool public canMint;
+    uint256 public totalMinted;
+    uint256 public basePrice;
+
+    // tokenId => additional price
+    mapping(uint256 => uint256) public boostedPrice;
+
+    constructor(
+        address _owner,
+        address _nftAddress,
+        bool _canMint,
+        uint256 _basePrice
+    ) {
+        owner = payable(_owner);
+        nft = CryptoChitahs(_nftAddress);
+        canMint = _canMint;
+        basePrice = _basePrice;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can perform this action");
+        _;
+    }
+
+    function setOwner(address _owner) public onlyOwner {
+        owner = payable(_owner);
+    }
+
+    function setBasePrice(uint256 _price) public onlyOwner {
+        basePrice = _price;
+    }
+
+    function setBoostedPrice(
+        uint256[] memory _tokenIDs,
+        uint256[] memory _boostedPrices
+    ) public onlyOwner {
+        require(
+            _tokenIDs.length == _boostedPrices.length,
+            "tokenIDs and boostedPrices array length doesn't match"
+        );
+
+        uint256 totalSupply = nft.totalSupply();
+        require(
+            _tokenIDs.length <= totalSupply,
+            "tokenID array is bigger than total supply"
+        );
+
+        for (uint256 i = 0; i < _tokenIDs.length; i++) {
+            uint256 tokenId = _tokenIDs[i];
+            uint256 price = _boostedPrices[i];
+
+            require(
+                tokenId > 0 && tokenId <= totalSupply,
+                string(
+                    abi.encodePacked(
+                        "tokenId array contains wrong token id at index of ",
+                        Strings.toString(i)
+                    )
+                )
+            );
+            boostedPrice[tokenId] = price;
+        }
+    }
+
+    function getBoostedPrice(uint256[] memory _tokenIDs)
+        public
+        view
+        onlyOwner
+        returns (uint256[] memory)
+    {
+        uint256 totalSupply = nft.totalSupply();
+        require(
+            _tokenIDs.length <= totalSupply,
+            "tokenID array is bigger than total supply"
+        );
+
+        uint256[] memory prices = new uint256[](_tokenIDs.length);
+
+        for (uint256 i = 0; i < _tokenIDs.length; i++) {
+            uint256 tokenId = _tokenIDs[i];
+
+            require(
+                tokenId > 0 && tokenId <= totalSupply,
+                string(
+                    abi.encodePacked(
+                        "tokenId array contains wrong token id at index of ",
+                        Strings.toString(i)
+                    )
+                )
+            );
+
+            prices[i] = boostedPrice[tokenId];
+        }
+        return prices;
+    }
+
+    function setMinting(bool _canMint) public onlyOwner {
+        canMint = _canMint;
+    }
+
+    function mint(uint256 tokenId) public payable returns (bool) {
+        require(canMint, "Minting is not started");
+
+        uint256 totalPrice = basePrice + boostedPrice[tokenId];
+        require(msg.value == totalPrice, "Incorrect price");
+
+        nft.mint(msg.sender, tokenId);
+
+        owner.transfer(msg.value);
+        boostedPrice[tokenId] = 0;
+        totalMinted += 1;
+
+        return true;
+    }
+}
---
+++ test/MarketPlace.test.js
@@ -0,0 +1,204 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("CryptoChitahs", () => {
+  let marketPlace;
+  let contractOwner, user1, user2, user3;
+  const NAME = "CryptoChitahs";
+  const SYMBOL = "CC";
+  const TOTALSUPPLY = 3974;
+  const BASEURI =
+    "https://cloudflare-ipfs.com/ipfs/QmXa9ZGusuAzTuXZviqcdu8fcF7ihpyjcUEw8Gk5n4gHgf/";
+  let cryptoChitahs;
+
+  beforeEach(async () => {
+    [contractOwner, user1, user2, user3] = await ethers.getSigners();
+
+    // deploy NFT collection
+    const CryptoChitahs = await ethers.getContractFactory("CryptoChitahs");
+    cryptoChitahs = await CryptoChitahs.deploy(
+      NAME,
+      SYMBOL,
+      TOTALSUPPLY,
+      contractOwner.address,
+      ethers.constants.AddressZero,
+      BASEURI
+    );
+
+    // deploy marketPlace
+    const MarketPlace = await ethers.getContractFactory("MarketPlace");
+    marketPlace = await MarketPlace.deploy(
+      contractOwner.address,
+      cryptoChitahs.address,
+      true,
+      ethers.utils.parseEther("1.25")
+    );
+
+    const tx = await cryptoChitahs
+      .connect(contractOwner)
+      .setMinter(marketPlace.address);
+    await tx.wait();
+  });
+
+  describe("Deployment", () => {
+    it("should deploy the marketplace contract with right arguments", async () => {
+      // contract's address should not be zero
+      expect(marketPlace.address).to.not.be.equal(ethers.constants.AddressZero);
+
+      // contract's owner should be contractOwner
+      expect(await marketPlace.owner()).to.be.equal(contractOwner.address);
+
+      expect(await marketPlace.nft()).to.be.equal(cryptoChitahs.address);
+
+      expect(await marketPlace.canMint()).to.be.equal(true);
+      expect(await marketPlace.totalMinted()).to.be.equal(0);
+      expect(await marketPlace.basePrice()).to.be.equal(
+        ethers.utils.parseEther("1.25")
+      );
+    });
+  });
+  describe("Setter", () => {
+    it("should set owner if called by owner", async () => {
+      const tx = await marketPlace
+        .connect(contractOwner)
+        .setOwner(user1.address);
+      await tx.wait();
+      expect(await marketPlace.owner()).to.be.equal(user1.address);
+    });
+
+    it("should not set owner if not called by owner", async () => {
+      await expect(
+        marketPlace.connect(user1).setOwner(user1.address)
+      ).to.be.revertedWith("Only owner can perform this action");
+    });
+
+    it("should set base price if called by owner", async () => {
+      const tx = await marketPlace
+        .connect(contractOwner)
+        .setBasePrice(ethers.utils.parseEther("1.5"));
+      await tx.wait();
+      expect(await marketPlace.basePrice()).to.be.equal(
+        ethers.utils.parseEther("1.5")
+      );
+    });
+
+    it("should set boosted prices if called by owner", async () => {
+      const tokenIds = [5, 10, 15, 20, 500, 1000];
+      const prices = [
+        ethers.utils.parseEther("0.5"),
+        ethers.utils.parseEther("0.75"),
+        ethers.utils.parseEther("1.0"),
+        ethers.utils.parseEther("1.25"),
+        ethers.utils.parseEther("1.5"),
+        ethers.utils.parseEther("1.75"),
+      ];
+      const tx = await marketPlace
+        .connect(contractOwner)
+        .setBoostedPrice(tokenIds, prices);
+      await tx.wait();
+
+      const boostedPrices = await marketPlace.getBoostedPrice(tokenIds);
+
+      // compare boosted prices
+      for (let i = 0; i < tokenIds.length; i++) {
+        expect(boostedPrices[i]).to.be.equal(prices[i]);
+      }
+    });
+
+    it("should set minting state", async () => {
+      // mint -> true
+      const tx1 = await marketPlace.setMinting(true);
+      await tx1.wait();
+      expect(await marketPlace.canMint()).to.be.equal(true);
+
+      // mint -> false
+      const tx2 = await marketPlace.setMinting(false);
+      await tx2.wait();
+      expect(await marketPlace.canMint()).to.be.equal(false);
+    });
+  });
+
+  describe.only("Mint", () => {
+    it("should mint a NFT if right amount is paid", async () => {
+      // with only base price
+      await (
+        await marketPlace.connect(user1).mint(2, {
+          value: ethers.utils.parseEther("1.25"),
+        })
+      ).wait();
+      expect(await cryptoChitahs.ownerOf(2)).to.be.equal(user1.address);
+
+      // with base price + boosted price
+      const tx = await marketPlace
+        .connect(contractOwner)
+        .setBoostedPrice([5], [ethers.utils.parseEther("2.0")]);
+      await tx.wait();
+
+      await (
+        await marketPlace.connect(user1).mint(5, {
+          value: ethers.utils.parseEther("3.25"),
+        })
+      ).wait();
+      expect(await cryptoChitahs.ownerOf(5)).to.be.equal(user1.address);
+    });
+
+    it("should not mint a NFT if right amount not paid", async () => {
+      // with only base price
+      await expect(
+        marketPlace.mint(2, {
+          value: ethers.utils.parseEther("1.1"),
+        })
+      ).to.be.revertedWith("Incorrect price");
+
+      // with base price + boosted price
+      const tx = await marketPlace
+        .connect(contractOwner)
+        .setBoostedPrice([5], [ethers.utils.parseEther("2.0")]);
+      await tx.wait();
+
+      await expect(
+        marketPlace.connect(user1).mint(5, {
+          value: ethers.utils.parseEther("1.25"),
+        })
+      ).to.be.revertedWith("Incorrect price");
+    });
+
+    it("should mint if canMint is true", async () => {
+      await (await marketPlace.setMinting(true)).wait();
+      await (
+        await marketPlace.connect(user1).mint(2, {
+          value: ethers.utils.parseEther("1.25"),
+        })
+      ).wait();
+      expect(await cryptoChitahs.ownerOf(2)).to.be.equal(user1.address);
+    });
+
+    it("should not mint if canMint is false", async () => {
+      await (await marketPlace.setMinting(false)).wait();
+
+      await expect(
+        marketPlace.connect(user1).mint(2, {
+          value: ethers.utils.parseEther("1.25"),
+        })
+      ).to.be.revertedWith("Minting is not started");
+    });
+
+    it("should increment total minted", async () => {
+      // totalMinted 1
+      await (
+        await marketPlace.connect(user1).mint(2, {
+          value: ethers.utils.parseEther("1.25"),
+        })
+      ).wait();
+      expect(await marketPlace.totalMinted()).to.be.equal(1);
+
+      // totalMinted 2
+      await (
+        await marketPlace.connect(user1).mint(5, {
+          value: ethers.utils.parseEther("1.25"),
+        })
+      ).wait();
+      expect(await marketPlace.totalMinted()).to.be.equal(2);
+    });
+  });
+});
