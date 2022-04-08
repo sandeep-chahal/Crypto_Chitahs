5d577a4896b44346bf53be717465300dc29798f5
sandeep-chahal <sandeepchahal573@gmail.com>
2022-04-08 10:26:10 +0530

    added search functionality

--- client/components/search/search.js
+++ client/components/search/search.js
@@ -1,13 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Link from "next/link";
 
 const Search = ({ onSearch }) => {
+  const [search, setSearch] = useState();
+
   return (
-    <div className="flex justify-end items-center">
+    <div className="flex justify-end items-center relative">
       <input
         className="rounded-md px-2 outline-slate-200 pr-7 hover:shadow-lg shadow-slate-900"
         placeholder="Search"
+        onChange={(e) => {
+          const value = parseInt(e.target.value);
+          if ((value > 0 && value <= 3974) || isNaN(value)) setSearch(value);
+        }}
+        value={search || ""}
       />
       <img src="/search.svg" className="w-4 absolute mr-2" onClick={onSearch} />
+      {!!search && (
+        <div className="absolute top-full left-0 bg-white w-full py-1 px-3 z-50 rounded-md font-semibold">
+          <ul>
+            {Array(5)
+              .fill(0)
+              .filter((_, i) => i + search <= 3974)
+              .map((_, index) => (
+                <li className="flex items-center my-3">
+                  <Link href={`/${search + index}`}>
+                    <a onClick={() => setSearch()}>
+                      Crypto Chitahs #{search + index}
+                    </a>
+                  </Link>
+                </li>
+              ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
