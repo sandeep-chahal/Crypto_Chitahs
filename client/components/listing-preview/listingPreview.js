e8f40666f057514e047037bf03fc5868723c1dcb
sandeep-chahal <sandeepchahal573@gmail.com>
2022-04-10 16:57:10 +0530

    fixed view all button

--- client/components/listing-preview/listingPreview.js
+++ client/components/listing-preview/listingPreview.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from "next/link";
 import Card from "../card";
 
 const ListingPreview = ({ nfts }) => {
@@ -6,13 +7,15 @@ const ListingPreview = ({ nfts }) => {
     <section className="px-5 md:px-20 lg:px-40">
       <h2 className="font-black text-2xl md:mb-6 flex items-center justify-between">
         <span>Listings</span>
-        <button className="flex items-center group text-base">
-          View All{" "}
-          <img
-            className="ml-2 w-4 transition-transform group-hover:translate-x-2 invert"
-            src="/right-arrow.svg"
-          />
-        </button>
+        <Link href="/browse">
+          <a className="flex items-center group text-base">
+            View All{" "}
+            <img
+              className="ml-2 w-4 transition-transform group-hover:translate-x-2 invert"
+              src="/right-arrow.svg"
+            />
+          </a>
+        </Link>
       </h2>
       <ul className="flex flex-wrap justify-between">
         {nfts.map((nft, i) => (
